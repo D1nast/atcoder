893bc2deb9871db973044209178b55d04c8d213f
Ishihara <almond1024r@gmail.com>
2024-10-15 23:35:31 +0900

    recursion

---
+++ diceStreak.rb
@@ -0,0 +1,19 @@
+class Stack
+  attr_accessor :data,:next
+  def initialize(data)
+    @data = data
+    @next = nil
+  end
+end
+
+def diceStreakGamble(player1,player2,player3,player4)
+    # 関数を完成させてください
+  
+
+end
+
+
+# "Winner:#{}won#{}by rolling#{}"
+
+# バーストしたらそこからカウント再開
+# 入力した前の事項を比較して、ダメだったら削除、カウント１からやり直し
