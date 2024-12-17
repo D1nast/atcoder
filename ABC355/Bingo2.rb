f6866c2e964ee3d653529c767195f64726f5b510
Ishihara <almond1024r@gmail.com>
2024-12-17 22:57:53 +0900

    問題演習

--- ABC355/Bingo2.rb
+++ ABC355/Bingo2.rb
@@ -1,29 +1,40 @@
-n, t = gets.split.map(&:to_i)
-row = Array.new(n, 0)
-col = Array.new(n, 0)
-dia1 = 0
-dia2 = 0
+n,t = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i)
 
+vertical = Array.new(n){0}
+horizontal = Array.new(n){0}
+leftdiag = n
+rightdiag = n
 
-t.times do |ti|
-  a = gets.to_i - 1
-  i = a / n
-  j = a % n
+(0...t).each do |i|
 
-  # ここでの `add` メソッドは C++ のラムダ式に相当します
-  add = lambda do |x|
-    x += 1
-    if x == n
-      puts ti + 1
-      exit
-    end
+  ai = a[i]
+  # puts "ai:#{ai}"
+
+  # 縦軸の判定
+  if ai % n == 0
+    vertical[ai/n-1] += 1
+  else
+    vertical[ai/n] += 1
+  end
+  # 横軸の判定
+  horizontal[ai%n-1] += 1
+  # 対角線軸の判定
+  leftdiag  -= 1 if (ai - 1) % (n + 1) == 0 || ai == 1 
+  rightdiag -= 1 if (ai - n) % (n - 1) == 0 || ai == n
+
+  puts "left:#{leftdiag} right:#{rightdiag}"
+  # ビンゴの判定
+  if horizontal[ai%n-1] == n || vertical[ai/n] == n || vertical[ai/n-1] == n || leftdiag == 0 || rightdiag == 0 
+    puts i+1
+    exit
   end
 
-  # row, col, dia1, dia2 の各対応するインデックスに対して add メソッドを呼び出す
-  add.call(row[i])
-  add.call(col[j])
-  add.call(dia1) if i == j
-  add.call(dia2) if i + j == n - 1
+  puts "vertical:#{vertical.inspect}"
+  puts "horizontal:#{horizontal.inspect}"
+  
 end
 
 puts -1
+
+# puts "#{vertical} #{horizontal}"
--- ABC384/c.rb
+++ ABC384/c.rb
@@ -1,53 +1,26 @@
-point = gets.split.map(&:to_i)
-list = []
-listToNumber = Hash.new
+n = 5
+a = gets.split.map(&:to_i)
 
-# 全ての組み合わせを試す
-(1...(1 << 5)).each do |i|
-  ans = Array.new(5){0}
-  (0...i).each do |shift|
-    # puts "#{i}回目のshift:#{shift}"
-    if (i >> shift ) & 1 == 1
-      # puts "#{i}回目 #{shift}"
-      ans[shift] = point[shift]
+ranking = []
+
+# 1 << n は 2^n なので、s = 1 から 2^n - 1 までループ
+(1...(1 << n)).each do |s|
+  score = 0
+  name = ''
+  
+  # 各ビットをチェックして部分集合を生成
+  (0...n).each do |i|
+    if s >> i & 1 == 1
+      score += a[i] 
+      name += ('A'.ord + i).chr
     end
   end
-  list << ans
+  
+  # スコアが大きい順、同じスコアなら名前の順に並べる
+  ranking << [-score, name]
 end
 
-list.each_with_index do |item,index|
-  listToNumber[index] = item.sum
+# ソートして出力.ソートは何も指定しないと、０要素目でソートされる
+ranking.sort.each do |score, name|
+  puts name
 end
-
-sorted = listToNumber.sort_by { |_, v| -v }.to_h
-puts sorted.inspect
-puts list.inspect
-# listToNumber.sort!
-
-
-# list.each do |num|
-#   str = ""
-
-#   num.each_with_index do |n,index|
-#     if n != 0
-#       case index
-#       when 0 then
-#         str += "A"
-#       when 1 then
-#         str += "B"
-#       when 2 then
-#         str += "C"
-#       when 3 then
-#         str += "D"
-#       else
-#         str += "E"
-#       end
-#     end
-#   end
-
-#   aaa << str
-
-# end
-
-# puts aaa.inspect
-
