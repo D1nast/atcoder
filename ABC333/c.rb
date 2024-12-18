f9f5e89df9394e6968d224773d8a81253f7f277c
Ishihara <almond1024r@gmail.com>
2024-12-18 23:38:15 +0900

    問題演習

--- ABC333/c.rb
+++ ABC333/c.rb
@@ -1,2 +1,17 @@
-n = gets.to_i
+require 'set'
 
+n = gets.to_i
+set = Set.new
+
+list = [1,11,111,1111,11111,111111,1111111,11111111,111111111,1111111111,11111111111,111111111111]
+
+list.each do |i|
+  list.each do |j|
+    list.each do |k|
+      set.add(i+j+k)
+    end
+  end
+end
+
+ans = set.sort
+puts ans[n-1]
