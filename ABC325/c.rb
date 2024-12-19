3fd32f3db012cf58a48b646c487a3d5a42d70d5b
Ishihara <almond1024r@gmail.com>
2024-12-19 23:00:09 +0900

    幅優先探索

--- ABC325/c.rb
+++ ABC325/c.rb
@@ -1,49 +1,48 @@
-# # 周囲８マスを調べる
-# def neighbor8(row,col,h,w)
-#   lst = []
-#   # [-1,0,1]は周囲のマスのインデックス
-#   [-1,0,1].each do |drow|
-#     [-1,0,1].each do |dcol|
-#       next if drow == 0 && dcol == 0 #drow == 0 、dcol==0は、周囲でなく中心のマスなので対象外
-#       nrow,ncol = row + drow, col + dcol #周囲マスのインデックスを定義
-#       lst << [nrow,ncol] if nrow >= 0 && nrow < h && ncol >= 0 && ncol < w #マスからはみ出なければOK
-#     end
-#   end
-#   return lst
-# end
+# 周囲８マスを調べる
+def neighbor8(row,col,h,w)
+  lst = []
+  # [-1,0,1]は周囲のマスのインデックス
+  [-1,0,1].each do |drow|
+    [-1,0,1].each do |dcol|
+      next if drow == 0 && dcol == 0 #drow == 0 、dcol==0は、周囲でなく中心のマスなので対象外
+      nrow,ncol = row + drow, col + dcol #周囲マスのインデックスを定義
+      lst << [nrow,ncol] if nrow >= 0 && nrow < h && ncol >= 0 && ncol < w #マスからはみ出なければOK
+    end
+  end
+  lst
+end
 
-# def bfs(srow,scol,h,w,s,seen)
-#   queue = [[srow,scol]]
-#   while !queue.empty?
-#     orow,ocol = queue.shift
-#     neighbor8(orow,ocol,h,w).each do |nrow,ncol|
-#       if !seen[nrow][ncol] && s[nrow][ncol] == '#'
-#         seen[nrow][ncol] == true
-#         queue.push([nrow,ncol])
-#       end
-#     end
-#   end
-# end
+def bfs(srow,scol,h,w,s,seen)
+  queue = [[srow,scol]]
+  while !queue.empty?
+    orow,ocol = queue.shift
+    neighbor8(orow,ocol,h,w).each do |nrow,ncol|
+      if !seen[nrow][ncol] && s[nrow][ncol] == '#'
+        seen[nrow][ncol] = true
+        queue.push([nrow,ncol])
+      end
+    end
+  end
+end
 
-# h,w = gets.split.map(&:to_i)
+h,w = gets.split.map(&:to_i)
 
-# s = Array.new(h) { gets.chomp }
-# seen = Array.new(h) { Array.new(w,false) }
+s = Array.new(h) { gets.chomp }
+seen = Array.new(h) { Array.new(w,false) }
+ans = 0
 
-# ans = 0
+(0...h).each do |row|
+  (0...w).each do |col|
+    if !seen[row][col] && s[row][col] == '#'
+      bfs(row,col,h,w,s,seen)
+      ans += 1
+    end
+  end
+end
 
-# (0...h).each do |row|
-#   (0...w).each do |col|
-#     if !seen[row][col] && s[row][col] == '#'
-#       bfs(row,col,h,w,s,seen)
-#       ans += 1
-#     end
-#   end
-# end
+puts ans
 
-# puts ans
-# 
-#
+
 #
 
 
