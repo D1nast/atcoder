939040937aa175021e47e6e51cb997e4f8e46dd2
Ishihara <almond1024r@gmail.com>
2024-12-23 23:57:52 +0900

    問題演習

--- ABC336/c.rb
+++ ABC336/c.rb
@@ -1,2 +1,11 @@
-puts 133/5
-puts 133%5
+n = gets.to_i - 1
+list = []
+ans = ""
+while n != 0 
+  list << (n % 5).to_s
+  n /= 5
+end
+
+list.reverse_each {|a| ans = ans + a }
+ans = ans.to_i
+puts ans*2
