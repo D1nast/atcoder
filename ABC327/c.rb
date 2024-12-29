2474a8d750869cfb4d960dfb49dd1e936f9ea329
Ishihara <almond1024r@gmail.com>
2024-12-29 19:23:31 +0900

    問題演習

--- ABC327/c.rb
+++ ABC327/c.rb
@@ -1,66 +1,53 @@
 arr = Array.new(10){[0,0,0,0,0,0,0,0,0,0]}
-arr3x3 = Array.new(10){[0,0,0]}
-hash = Hash.new()
 # 9x9のマス目
 (1..9).each do |i| 
-  hash[i] = 0
   row = [0] + gets.split.map(&:to_i)
   arr[i] = row
 end
-
-#　列と行の要件確認
-(1..9).each do |i|
-  # 行の確認
-  if  arr[i].sum != 45
-    puts "No"
-    exit
+# 行の確認
+arr.each_with_index do |item,index|
+  next if index == 0
+  check = Array.new(10){0}
+  (1..9).each { |i|check[item[i]] += 1 }
+  check.each_with_index do |i,index|
+    next if index == 0
+    if i != 1
+      puts "No"
+      exit
+    end
   end
-  count = 0
-
-  (1..9).each do |j|
-    count += arr[j][i]
-    hash[arr[i][j]] += 1
-  end
-
-  if count != 45
-    puts "No"
-    exit
-  end
-
 end
-
+# 列確認
 (1..9).each do |i|
-  sum = 0
-  (1..9).each do |j|
-    sum += arr[i][j]
-    if j % 3 == 0
-      arr3x3[i][j/3-1] = sum
-      sum = 0
+  check = Array.new(10){0}
+  (1..9).each { |j| check[arr[j][i]] += 1 }
+  check.each_with_index do |item,index|
+    next if index == 0
+    if item != 1
+      puts "No"
+      exit
     end
   end
 end
 
-# 3x3 を９回繰り返す
+# 3x3確認
+# 4重ループで3*3のブロックを探索
 [1,4,7].each do |i|
-  one = arr3x3[i][0]
-  two = arr3x3[i][1]
-  three = arr3x3[i][2]
-  (i+1...i+3).each do |j|
-    one += arr3x3[j][0]
-    two += arr3x3[j][1]
-    three += arr3x3[j][2]
-  end
-  if one != 45 || two != 45 || three != 45
-    puts "No"
-    exit
-  end
-end
-
-hash.each do |key,value|
-  if value != 9
-    puts "No"
-    exit
+  [1,4,7].each do |j|
+    check = Array.new(10){0}
+    [0,1,2].each do |ni|
+      [0,1,2].each do |nj|
+        row,column =  i + ni, j + nj
+        check[arr[row][column]] += 1
+      end
+    end
+    check.each_with_index do |item,index|
+      next if index == 0
+      if item != 1
+        puts "No"
+        exit
+      end
+    end
   end
 end
-
 puts "Yes"
