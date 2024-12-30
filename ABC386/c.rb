05126ece630593b2aa7d98f4128c406b2d627ced
Ishihara <almond1024r@gmail.com>
2024-12-30 14:02:55 +0900

    問題演習

--- ABC386/c.rb
+++ ABC386/c.rb
@@ -1,29 +1,36 @@
 k = gets.to_i
-s,t = " " + gets.chomp , " " + gets.chomp
+s,t = gets.chomp.chars ,gets.chomp.chars
 slen,tlen = s.length,t.length
 len = [slen,tlen].max
 count,nilc = 0 , 0
 insert,delete = s.dup ,s.dup
 
-(1...len).each do |i|
+(0...len).each do |i|
   if s[i].nil? || t[i].nil?
     nilc += 1
   elsif s[i] != t[i]
     count += 1
   end
 end
-(1...len).each do |i|
+(0...len).each do |i|
   if s[i] != t[i]
-    insert.insert(i,t[i])
-    delete.slice!(i)
+    # 文字数がS<Tの時は削除のみ
+    if slen - tlen < 0
+      insert.insert(i,t[i])     
+    else
+    # 文字数がS>Tの時は削除のみ
+      delete.slice!(i)
+    end   
     break
   end
 end
 
-if count <= k || insert == t || delete == t
+if (count <= k && nilc <= k) || insert == t || delete == t
   puts "Yes"
 else
   puts "No"
 end
-# puts insert,delete
+
+# puts s.inspect,t.inspect
+# puts insert.inspect,delete.inspect
 # puts "count:#{count} nilc:#{nilc}"
