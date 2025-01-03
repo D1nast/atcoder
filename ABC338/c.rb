6ab041d8fd5cbb1f94d131caa680ee81b22108ae
Ishihara <almond1024r@gmail.com>
2025-01-04 01:37:54 +0900

    問題演習

---
+++ ABC338/c.rb
@@ -0,0 +1,32 @@
+n = gets.to_i
+q = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i)
+b = gets.split.map(&:to_i)
+ans = 0
+turn = Float::INFINITY
+upperB = Float::INFINITY
+
+(0...n).each do |i|
+  turn = [ q[i]/a[i],turn ].min if a[i] != 0
+  upperB = [ upperB,q[i]/b[i] ].min if b[i] != 0
+end
+
+(0..turn).each do |i|
+  cook = 0
+  minB = Float::INFINITY
+  (0...n).each do |j|
+    if b[j] == 0
+      bn = upperB
+    else
+      bn = ( q[j] - a[j]* i ) / b[j]
+      bn = upperB if bn > upperB
+    end
+    minB = [ minB,bn ].min
+  end
+  cook = minB + i
+  ans = [ ans,cook ].max
+end
+
+puts ans
+
+# 計算量
--- seisen100/動的計画法/bitDP/dpl2a.rb
+++ seisen100/動的計画法/bitDP/dpl2a.rb
@@ -6,19 +6,21 @@ d = Array.new(v) { Array.new(v, INF) }
 
 # グラフの辺の情報を読み込む
 e.times do
-  s, t, distance = gets.split.map(&:to_i)  # Dをdistanceに変更
-  d[s][t] = distance  # distanceという変数を使う
+  s, t, distance = gets.split.map(&:to_i)  
+  d[s][t] = distance 
 end
 
 # DP配列の初期化
 dp = Array.new(1 << v) { Array.new(v, INF) }
 dp[0][0] = 0
+puts "d:#{d.inspect}"
+puts "dp:#{dp.inspect} dp.size:#{dp.size}"
 
 # ビット全探索による動的計画法
 (0...(1 << v)).each do |i|
   (0...v).each do |j|
     (0...v).each do |k|
-      if (i & (1 << k)) == 0
+      if (i & (1 << k)) == 0 # 次のノードが未訪問かどうか（iのbit≠k番目のビット）
         dp[i | (1 << k)][k] = [dp[i | (1 << k)][k], dp[i][j] + d[j][k]].min
       end
     end
@@ -28,3 +30,4 @@ end
 # 結果を表示
 result = dp[(1 << v) - 1][0]
 puts result == INF ? -1 : result
+puts "dp:#{dp.inspect} dp.size:#{dp.size}"
