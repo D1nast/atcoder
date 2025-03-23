fbf4c126f9f0dd3f2a9afbb5381c7051dbc0489f
Ishihara <almond1024r@gmail.com>
2025-03-24 00:43:45 +0900

    AC

--- ABC396/c.rb
+++ ABC396/c.rb
@@ -1,40 +1,21 @@
 n,m = gets.split.map(&:to_i)
 b = gets.split.map(&:to_i).sort.reverse
 w = gets.split.map(&:to_i).sort.reverse
-finN = n-1
 sum = 0
 (0...n).each do |i|
-    if b[i] <= 0
-        finN = [ i,finN ].min
-        break
-    else
+    if b[i] > 0
         sum += b[i]
-    end
-end
-
-# puts "n終了後:#{sum} finN:#{finN}"
-
-(0...m).each do |i|
-    break if i > n-1
-    if  w[i] > 0
-        if finN > i
-            sum += w[i]
-        else
-            if w[i] + b[i] > 0
-                # puts w[i] + b[i]
-                sum += (w[i] + b[i])
-                finN = i
-            else
-                break
-            end
-        end
+        sum += w[i] if !(w[i].nil?) && w[i] > 0 
     else
-        break
+        break if w[i].nil?
+        if b[i] + w[i] > 0
+            sum += (b[i]+w[i])
+        else
+            break
+        end
     end
 end
 
 puts sum
-# puts b.inspect
-# puts w.inspect
-# bがマイナスの時は差し引きしてプラスならOK
-# wはマイナスになった時点でボールピック中止
+puts b.inspect
+puts w.inspect
