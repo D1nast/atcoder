1739edf63c1f5dc59467f7b988f34d7bc77f183c
Ishihara <almond1024r@gmail.com>
2025-04-06 19:19:22 +0900

    問題演習

--- ABC384/d.rb
+++ ABC384/d.rb
@@ -1,31 +1,30 @@
-n,s = gets.split.map(&:to_i)
-A = gets.split.map(&:to_i)
-sum = A.sum
-targetF = s % sum
-targetL = s % sum
-first = 0
-(0...n).each do |i|
-  first += A[i]
-  if first == s
-    puts "Yes"
-    exit
+require 'set'
+
+n , s = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i)
+sum = a.sum
+target = s%sum
+cumulative = Array.new(n*2)
+set = Set.new()
+set.add(0)
+
+(0...n*2).each do |i|
+  if i < a.size
+    cumulative[i] = a[i] if i == 0
+    cumulative[i] = a[i] + cumulative[i-1] if i != 0
+  else
+    cumulative[i] = a[i-a.size] + cumulative[i-1]
   end
 end
 
-puts "target:#{targetL} sum:#{sum}"
-(0...n).each do |i|
-  targetF -= A[i]
-  if targetF == 0
-    puts "Yes"
-    exit
-  end
-end
-(0...n).reverse_each do |i|
-  targetL -= A[i]
-  if targetL == 0
-    puts "Yes"
-    exit
-  end
-end
+cumulative.each { |element| set.add(element) }
 
+cumulative.each do |e|
+  if set.include?(e-target)
+    puts "Yes"
+    exit
+  end
+end  
 puts "No"
+# puts cumulative.inspect
+# puts target
