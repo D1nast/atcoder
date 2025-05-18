a0aeed4b9d0bbfc376986273e77b084f57224262
Ishihara <almond1024r@gmail.com>
2025-05-18 18:23:21 +0900

    問題演習

--- ABC350~/ABC406/c.rb
+++ ABC350~/ABC406/c.rb
@@ -1,14 +1,30 @@
-require 'set'
 N = gets.to_i
 P = gets.split.map(&:to_i)
-h_set = Hash.new()
-l_set = Hash.new()
-
-(1...P.size-1).each do |i|
-  h_set[P[i]] = i if P[i-1] < P[i] && P[i] > P[i+1]
-  l_set[P[i]] = i if P[i-1] > P[i] && P[i] < P[i+1]
+check = []
+rle = []
+ans = 0
+count = 0
+(1...P.size).each do |i|
+  if P[i] >P[i-1]
+    check << 0
+  else
+    check << 1
+  end
+end
+(0...check.size).each do |i|
+  if check[i] == 0
+    count += 1
+  else
+    rle << count if count > 0
+    count = 0
+  end
+  rle << count if i==check.size-1 && count > 0
 end
 
-(1...P.size-3).each do |i|
-    
+(1...rle.size).each do |i|
+  ans += rle[i-1]*rle[i]
 end
+
+# puts rle.inspect
+# puts check.inspect
+puts ans
