29dc51cbe2f2a1aa12fe06bc0f71842519b5d0ac
Ishihara <almond1024r@gmail.com>
2025-06-03 23:09:23 +0900

    問題演習

--- ABC350~/ABC404/d.py
+++ ABC350~/ABC404/d.py
@@ -1,2 +1,18 @@
 n, m = map(int, input().split())
+c = map(int,input().split())
+zoo = [[] for _ in range(n)]
+animal = [[]for _ in range(m)]
+
+for i in range(m):
+    value =   list(map(int, input().split()))
+    for j in value[1:]:
+        # j -= 1
+        zoo[j-1].append(i)
+        animal[i].append(j-1)
+
+
+print (animal)
+print (zoo)
+
+
 
