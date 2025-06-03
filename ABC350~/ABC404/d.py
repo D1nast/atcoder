2e25d48362ceeacca75f08719a7e6d580d8141b0
Ishihara <almond1024r@gmail.com>
2025-06-04 00:09:44 +0900

    問題演習

--- ABC350~/ABC404/d.py
+++ ABC350~/ABC404/d.py
@@ -1,18 +1,28 @@
+# 3進数で解く
+from itertools import product
+import math
 n, m = map(int, input().split())
-c = map(int,input().split())
+c = list(map(int,input().split()))
 zoo = [[] for _ in range(n)]
-animal = [[]for _ in range(m)]
+ans = math.inf
 
 for i in range(m):
     value =   list(map(int, input().split()))
     for j in value[1:]:
-        # j -= 1
         zoo[j-1].append(i)
-        animal[i].append(j-1)
+for pair in product([0,1,2],repeat=n):
+    sum = 0
+    animal = [ 0 for _ in range(m)]
+    # print("組み合わせ",pair)
+    for index , num in enumerate(pair):
+        sum += c[index]*num
+        for element in zoo[index]:
+            animal[element] += num
+    if all(ele >= 2 for ele in animal):
+        ans = min([ans,sum])
+    # print("料金",sum)
+    # print("animal",animal)
 
 
-print (animal)
-print (zoo)
 
-
-
+print(ans)
